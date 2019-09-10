af021ca0db3ff83ec3e368d31dc16ba4d9fbc370
tteogi <tteogi82@gmail.com>
2019-09-10 14:27:30 +0900

    converts to .Net core from CodeWriter (https://github.com/SaladLab/CodeWriter)

--- build.fsx
+++ build.fsx
@@ -1,61 +1,113 @@
-﻿#I @"packages/FAKE/tools"
-#I @"packages/FAKE.BuildLib/lib/net451"
-#r "FakeLib.dll"
-#r "BuildLib.dll"
+﻿#r "paket:
+nuget Fake.Core.ReleaseNotes
+nuget Fake.Core.Xml
+nuget Fake.DotNet.Cli
+nuget Fake.DotNet.Paket
+nuget Fake.Tools.Git
+nuget Fake.Core.Process
+nuget Fake.IO.FileSystem
+nuget Fake.DotNet.MSBuild
+nuget Fake.DotNet.NuGet
+nuget Fake.Core.Target //"
+#load "./.fake/build.fsx/intellisense.fsx"
 
-open Fake
-open BuildLib
+open Fake.Core
+open Fake.IO
+open Fake.Core.TargetOperators
+open System
+open System.IO
+open Fake.DotNet
+open Fake.DotNet.NuGet
 
-let solution = 
-    initSolution
-        "./CodeWriter.sln" "Release" 
-        [ { emptyProject with Name = "CodeWriter" 
-                              Folder = "./core/CodeWriter" } ]
 
-Target "Clean" <| fun _ -> cleanBin
+let nugetApiKye = "oy2ejb6lhkzh7d2zav5atjfiocskyyeuxdazii2znduk3e"
+let buildDir = "./build/"
+let testDir  = "./test/"
+let configuration = DotNet.BuildConfiguration.Release
 
-Target "AssemblyInfo" <| fun _ -> generateAssemblyInfo solution
 
-Target "Restore" <| fun _ -> restoreNugetPackages solution
+// Properties
+let summary = "Helper class for generating code concisely."
+let copyright = "Copyright © 2019 tteogi"
+let description = """
+Helper class for generating code concisely.
+This package converts to .Net core from CodeWriter (https://github.com/SaladLab/CodeWriter).
+"""
+let authors = ["Ten Y"]
+let owner = "Ten Y"
+let tags = "code write builder generator"
+let solutionFile = "CodeWriter"
+let nugetVersion = "1.0.1";
+let gitHome = "https://github.com/tteogi"
+let gitName = "CodeWriter"
+let project = "CodeWriter.Core"
+let projectUrl = sprintf "%s/%s" gitHome gitName
+let licenceUrl = "https://raw.githubusercontent.com/tteogi/tteogi.github.io/master/MIT-LICENSE"
 
-Target "Build" <| fun _ -> buildSolution solution
+Target.create "Pack" (fun _ ->
+    let pack project =
 
-Target "Test" <| fun _ -> testSolution solution
+        let projectPath = sprintf "core/%s/%s.csproj" project project
+        let args =
+            let defaultArgs = MSBuild.CliArguments.Create()
+            { defaultArgs with
+                      Properties = [
+                          "Title", project
+                          "PackageVersion", nugetVersion
+                          "Authors", (String.Join(" ", authors))
+                          "Owners", owner
+                          "PackageRequireLicenseAcceptance", "false"
+                          "Description", description
+                          "Summary", summary
+                          "Copyright", copyright
+                          "PackageTags", tags
+                          "PackageProjectUrl", projectUrl
+                          "PackageLicenseUrl", licenceUrl
+                      ] }
 
-Target "Cover" <| fun _ -> coverSolution solution
-    
-Target "Coverity" <| fun _ -> coveritySolution solution "SaladLab/CodeWriter"
+        DotNet.pack (fun p ->
+            { p with
+                  Configuration = configuration
+                  OutputPath = Some "./build"
+                  MSBuildParams = args
+              }) projectPath
 
-Target "PackNuget" <| fun _ -> createNugetPackages solution
+    pack "CodeWriter.Core"
+)
 
-Target "Pack" <| fun _ -> ()
 
-Target "PublishNuget" <| fun _ -> publishNugetPackages solution
+// Targets
+Target.create "Clean" (fun _ ->
+  Shell.cleanDirs [buildDir; testDir]
+)
 
-Target "Publish" <| fun _ -> ()
+Target.create "Default" (fun _ ->
+  Trace.trace "Hello World from FAKE"
+)
 
-Target "CI" <| fun _ -> ()
+Target.create "Push" (fun _ ->
+    let setNugetPushParams (defaults:NuGet.NuGetPushParams) =
+            { defaults with
+                Source = Some "https://api.nuget.org/v3/index.json"
+                ApiKey = Some nugetApiKye
 
-Target "Help" <| fun _ -> 
-    showUsage solution (fun _ -> None)
+             }
+    let setParams (defaults:DotNet.NuGetPushOptions) =
+            { defaults with
+                PushParams = setNugetPushParams defaults.PushParams
+             }
 
+    IO.Directory.EnumerateFiles(buildDir, "*.nupkg", SearchOption.TopDirectoryOnly)
+    |> Seq.iter (fun nupkg ->
+        DotNet.nugetPush setParams nupkg
+    )
+)
+
+open Fake.Core.TargetOperators
 "Clean"
-  ==> "AssemblyInfo"
-  ==> "Restore"
-  ==> "Build"
-  ==> "Test"
+  ==> "Pack"
+  ==> "Push"
+  ==> "Default"
 
-"Build" ==> "Cover"
-"Restore" ==> "Coverity"
-
-let isPublishOnly = getBuildParam "publishonly"
-
-"Build" ==> "PackNuget" =?> ("PublishNuget", isPublishOnly = "")
-"PackNuget" ==> "Pack"
-"PublishNuget" ==> "Publish"
-
-"Test" ==> "CI"
-"Cover" ==> "CI"
-"Publish" ==> "CI"
-
-RunTargetOrDefault "Help"
+// start build
+Target.runOrDefault "Default"
